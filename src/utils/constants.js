15b8c3d9a646495507cc2684e4479624816c7e72
Nahi002 <nahomdebele002@gmail.com>
2024-10-28 23:05:17 +0300

    constants

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+/**
+ * The base URL for fetching data from the Bacon Ipsum API.
+ * 
+ * @constant {string} BASE_URL
+ * @default "https://baconipsum.com/api"
+ * @see https://baconipsum.com/api/
+ */
+export const BASE_URL = "https://baconipsum.com/api";
