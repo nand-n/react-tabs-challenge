167a8db971eddd619f3e12639ea53a19967622a3
Nahi002 <nahomdebele002@gmail.com>
2024-10-29 20:45:54 +0300

    app test fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Title/i);
+  const linkElement = screen.getByText(/Tab 2/i);
   expect(linkElement).toBeInTheDocument();
 });
